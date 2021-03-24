045839dde2c05c2aac4f6c2db23d4483245eb35b
Fuentes Peralta Nelson Kevin <fuenteskevin543@gmail.com>
2021-03-24 18:01:12 -0500

    configuracion con posgres

---
+++ mies/mies/settings/dev.py
@@ -0,0 +1,38 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'db_mies',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '1234567890',
+
+        'HOST': 'localhost',
+
+        'PORT': '',
+
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Configuracion para tratamiento de las imagenes
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ mies/mies/settings/prod.py
@@ -0,0 +1,36 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': '',
+
+        'USER': '',
+
+        'PASSWORD': '',
+
+        'HOST': '',
+
+        'PORT': '',
+
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Configuracion para tratamiento de las imagenes
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
