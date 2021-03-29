3dc4ad2fd5b3a114ad104983bad5c5aeba9b5586
Fuentes Peralta Nelson Kevin <fuenteskevin543@gmail.com>
2021-03-29 08:26:51 -0500

    listo

--- mies/usuario/views.py
+++ mies/usuario/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-def login_view(request):
-    return render(request, 'usuario/login.html')
 
 @login_required
 def dashboardUsuario_view(request):
